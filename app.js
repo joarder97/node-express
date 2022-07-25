48ac81d080ca9189cf274c575b2f0c450deb9907
joarder97 <masudul.pallab99@gmail.com>
2022-07-25 18:24:48 +0600

    file system sync

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
-const os = require('os');
+const {readFileSync, writeFileSync} = require('fs')
 
-//info about current user
-
-const user = os.userInfo();
-console.log(user);
-
-// method returns the systems uptime in seconds
-console.log(`System uptime: ${os.uptime()} seconds`);
-
-const currentOS = {
-    name: os.type(),
-    release: os.release(),
-    totalMem: os.totalmem(),
-    freeMem: os.freemem(),
-}
-console.log(currentOS);
