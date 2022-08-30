f823400575a24e0ed517d812be38969bea0caca5
root <masudul.pallab99@gmail.com>
2022-08-31 03:03:11 +0600

    newsletter subscription

---
+++ Newsletter-app/app.js
@@ -0,0 +1,84 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+require('dotenv').config({path:__dirname+'/.env'})
+
+const app = express();
+app.use(bodyParser.urlencoded({
+        extended: true 
+    }));
+
+app.use(express.static("public")); 
+
+mailchimp.setConfig({
+    apiKey: process.env.API_KEY,
+    server: process.env.SERVER,
+});
+
+
+app.get("/", (req, res)=> {
+    res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res)=> {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const run = async () => {
+        console.log(process.env.USER_ID);
+        const response = await mailchimp.lists.batchListMembers(process.env.USER_ID, jsonData);
+        const error = JSON.stringify(response);
+        var json = JSON.parse(error);
+        // console.log(json.error_count);
+        if(json.error_count > 0){
+            res.sendFile(__dirname + "/failure.html");
+        } else{
+            res.sendFile(__dirname + "/success.html");
+        }
+
+    };
+
+    try{
+        run();
+    } catch(err){
+        console.log(err);
+    } 
+
+
+    // console.log(request);
+
+    // request.write(jsonData);
+    // request.end();
+});
+
+app.post("/failure", (req, res) => {
+    res.redirect("/");
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+} );
+
+// 33a518a93e77c114a2b4e5b33eba4227-us10
+// 97c518ea44
