534d89ac892f5670bb26d315cbdd8c42de0796c6
root <masudul.pallab99@gmail.com>
2022-08-31 07:14:55 +0600

    key

--- Newsletter-app/app.js
+++ Newsletter-app/app.js
@@ -69,6 +69,6 @@ app.post("/failure", (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000");
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started");
 } );
