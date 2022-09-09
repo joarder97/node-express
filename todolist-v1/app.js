57e832dce78185c95d0ba5581a15c82dc7e9dd35
root <masudul.pallab99@gmail.com>
2022-09-09 22:54:46 +0600

    todo ejs

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,43 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
+var newItems = ["Breakfast", "Exercise"];
+
+app.get("/", (req, res) => {
     var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
 
-        switch (currentDay) {
-            case 0:
-            day = "Sunday";
-            break;
-            case 1:
-            day = "Monday";
-            break;
-            case 2:
-            day = "Tuesday";
-            break;
-            case 3:
-            day = "Wednesday";
-            break;
-            case 4:
-            day = "Thursday";
-            break;
-            case 5:
-            day = "Friday";
-            break;
-            case 6:
-            day = "Saturday";
-            break;
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-        default:
-            break;
-    }
+    var day = today.toLocaleDateString("en-US", options);
+
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItem: newItems
     });
+});
+
+app.post("/", (req,res)=>{
+    var newItem = req.body.newItem;
+    newItems.push(newItem);
+    res.redirect("/");
 });
 
 app.listen(3000, () => {
